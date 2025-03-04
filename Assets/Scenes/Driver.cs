31a6cddbcdcf64c8f18198876b3be1f10302be3f
YoungMoo Kim <rladuan612@gmail.com>
2025-03-04 18:10:49 +0900

    use transform

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
-{
+{   
+    [SerializeField]
+    float steerSpeed = 0.1f;
+
+    [SerializeField]    
+    float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {   
@@ -14,7 +19,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        transform.Rotate(0,0,0.1f); // 오브젝트 회전
-        transform.Translate(0,0.01f,0); // 오브젝트 이동 
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0,0,-steerAmount); // 오브젝트 회전
+        transform.Translate(0,moveAmount,0); // 오브젝트 이동 
     }
 }
